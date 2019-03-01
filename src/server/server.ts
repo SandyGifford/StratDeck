fba71b3389b7c3326951eb5dc73326a960dc8eec
Sandy Gifford <sanford.r.gifford@gmail.com>
2019-03-01 17:53:44 -0500

    update port for heroku

--- src/server/server.ts
+++ src/server/server.ts
@@ -9,7 +9,9 @@ import routing from "./routing";
 const app = express();
 const server: Server = require("http").Server(app);
 
+const port = parseInt(process.env.PORT) || 3000;
+
 SocketStuff(server);
 app.use(routing);
 
-server.listen(3000, "0.0.0.0");
+server.listen(port, "0.0.0.0");
